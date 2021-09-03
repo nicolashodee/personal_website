f87bd581bd9782488d6f83f520eda80ced3de60c
Nicolas HODEE <accounts@nicolashodee.com>
2021-09-03 18:26:00 +0200

    added animations and corrected some bugs

---
+++ retouch/anim-retouch.js
@@ -0,0 +1,43 @@
+const retouchBlock = gsap.utils.toArray('.retouch-block');
+const folderItems = document.querySelectorAll('.folders li');
+const folderBlock = document.querySelector('.folders');
+console.log(folderBlock);
+
+
+// ANIMATION FOR RETOUCH SLIDER
+
+retouchBlock.forEach(block => {
+  const animRetouch = gsap.timeline({ defaults: {ease: 'none'} }); 
+  animRetouch.from(".retouch-text", {x:"+100%", opacity: 0, duration: 0.5});
+  
+  ScrollTrigger.create({
+    trigger: block,
+    start: "top 60%",
+    onEnter: () => animRetouch.play()
+  });
+  
+  ScrollTrigger.create({
+    trigger: block,
+    start: "top 60%",
+    onLeaveBack: () => animRetouch.reverse()
+  });
+});
+
+// ANIMATION FOR FOLDERS GRID
+
+folderItems.forEach(block => {
+  const animFolders = gsap.timeline({ defaults: {ease: 'none'} }); 
+  animFolders.from("li", {x:"+100%", opacity: 0, duration: 0.5});
+  
+  ScrollTrigger.create({
+    trigger: block,
+    start: "top 80%",
+    onEnter: () => animFolders.play()
+  });
+  
+  ScrollTrigger.create({
+    trigger: block,
+    start: "top 80%",
+    onLeaveBack: () => animFolders.reverse()
+  });
+});
